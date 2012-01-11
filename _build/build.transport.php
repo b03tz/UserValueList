8bc41158a6491c554adf354e38daa7e0a3d24208
Mark Hamstra <hello@markhamstra.com>
2012-01-11 15:52:12 +0100

    Cleanup

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -71,6 +71,7 @@ $modx->setLogTarget('ECHO'); echo '<pre>'; flush();
 
 $modx->loadClass('transport.modPackageBuilder','',false, true);
 $builder = new modPackageBuilder($modx);
+//$builder->directory = dirname(dirname(__FILE__)).'/_packages/';
 $builder->createPackage(PKG_NAME_LOWER,PKG_VERSION,PKG_RELEASE);
 $builder->registerNamespace(PKG_NAME_LOWER,false,true,'{core_path}components/'.PKG_NAME_LOWER.'/');
 $modx->getService('lexicon','modLexicon');
